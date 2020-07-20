506771aadc0048b1438b23ad7525a8a5dd4021fe
Javier Ramos <jaramosperez85@gmail.com>
2020-07-20 14:29:38 -0400

    Agregando Tasks

--- react-tasks-app/src/App.tsx
+++ react-tasks-app/src/App.tsx
@@ -1,27 +1,41 @@
 import React, { Fragment, useState  } from 'react';
 
 type FormElement = React.FormEvent<HTMLFontElement>
+interface ITask {
+  name: string;
+  done: boolean;
+}
 
 function App(): JSX.Element {
 
-  const [newTask, setNewTask] = useState('');
+const [newTask, setNewTask] = useState<string>('');
+const [tasks, setTasks] = useState<ITask[]>([])
 
 const handleSubmit = (e: FormElement) => {
   e.preventDefault();
-  console.log(newTask);
+  addTask(newTask);
+  setNewTask('');
+}
 
+const addTask = (name: string) => {
+  const newTasks: ITask[] = [...tasks, {name, done: false}]
+  setTasks(newTasks)
 }
 
   return (
     <Fragment>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setNewTask(e.target.value)}/>
+        <input type="text" onChange={e => setNewTask(e.target.value)} value={newTask}/>
         <button>
           Guardar
         </button>
       </form>
+      { tasks.map((t: ITask)=> {
+        return <h1>{t.name}</h1>
+      })
+      }
+
     </Fragment>
   );
 }
-
 export default App;
